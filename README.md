8350f162dcfee89d9016285bb1cbcf7fc6a21e8f
Eslam A. Hefnawy <eahefnawy@gmail.com>
2015-09-27 18:37:14 +0200

    pushing

---
+++ README.md
@@ -0,0 +1,7 @@
+# 💳 awsm-stripe-webhook
+
+In your JAWS project root directory, run:
+
+```
+jaws module install https://github.com/eahefnawy/awsm-stripe-webhook
+```
---
+++ webhook/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * Export for lambda handler
+ * @param event
+ * @param context
+ * @param cb
+ */
+exports.run = function(event, context, cb) {
+  /**
+   *
+   *
+   * Write your callback code here
+   *
+   *
+  */
+  console.log(event);
+};
