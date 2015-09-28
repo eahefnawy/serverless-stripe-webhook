8091e0fbb73a897d0bdd0d208f8a45a9834aa925
Eslam A. Hefnawy <eahefnawy@gmail.com>
2015-09-28 17:07:09 +0200

    finished & tested

--- webhook/index.js
+++ webhook/index.js
@@ -10,9 +10,13 @@ exports.run = function(event, context, cb) {
   /**
    *
    *
-   * Write your callback code here
+   *
+   * Write your callback code here. Make sure you validate the request
+   * by checking the event ID with slack before processing.
+   *
    *
    *
   */
-  return cb(null, {"data": "YESS"});
+  console.log(event.id);
+  return cb(null, {"message": "success"});
 };
